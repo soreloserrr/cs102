e9d6221b833638c0bfb702120974af80ed04a32e
soreloserrr <ekaterina.buyakova00@mail.ru>
2022-11-06 17:05:56 +0300

    Реализована функция decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -12,3 +12,19 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
             ciphertext += letters
 
     return ciphertext
+
+
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    plaintext = ""
+    for letters in ciphertext:
+        if letters.isalpha():
+            stay_in_alphabet = ord(letters) - shift
+            if stay_in_alphabet < ord('a') and letters.lower() == letters or stay_in_alphabet < ord(
+                    'A') and letters.upper() == letters:
+                stay_in_alphabet += 26
+            final_letter = chr(stay_in_alphabet)
+            plaintext += final_letter
+        else:
+            plaintext += letters
+
+    return plaintext
