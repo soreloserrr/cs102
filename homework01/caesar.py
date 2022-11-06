1b49b4c6e4f8cc101074d9e6956c9cda8a9fa549
soreloserrr <ekaterina.buyakova00@mail.ru>
2022-11-06 17:12:57 +0300

    код исправлен в соответствии с pep8

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -3,8 +3,12 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     for letters in plaintext:
         if letters.isalpha():
             stay_in_alphabet = ord(letters) + shift
-            if stay_in_alphabet > ord('z') and letters.lower() == letters or stay_in_alphabet > ord(
-                    'Z') and letters.upper() == letters:
+            if (
+                    stay_in_alphabet > ord("z")
+                    and letters.lower() == letters
+                    or stay_in_alphabet > ord("Z")
+                    and letters.upper() == letters
+            ):
                 stay_in_alphabet -= 26
             final_letter = chr(stay_in_alphabet)
             ciphertext += final_letter
@@ -19,8 +23,12 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     for letters in ciphertext:
         if letters.isalpha():
             stay_in_alphabet = ord(letters) - shift
-            if stay_in_alphabet < ord('a') and letters.lower() == letters or stay_in_alphabet < ord(
-                    'A') and letters.upper() == letters:
+            if (
+                    stay_in_alphabet < ord("a")
+                    and letters.lower() == letters
+                    or stay_in_alphabet < ord("A")
+                    and letters.upper() == letters
+            ):
                 stay_in_alphabet += 26
             final_letter = chr(stay_in_alphabet)
             plaintext += final_letter
