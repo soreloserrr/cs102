83b933b89d90df72593de18901ac8ea23dae7b1b
soreloserrr <ekaterina.buyakova00@mail.ru>
2022-11-06 18:01:19 +0300

    Рефлизована функция поиска НОД

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,3 +1,6 @@
+import random
+
+
 def is_prime(n: int) -> bool:
     """
     >>> is_prime(2)
@@ -11,3 +14,55 @@ def is_prime(n: int) -> bool:
         if n % i == 0:
             return False
     return True
+
+
+def generate_keypair(p: int, q: int) -> tuple[tuple[int, int], tuple[int, int]]:
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+
+    # n = pq
+    n = p * q
+
+    # phi = (p-1)(q-1)
+    phi = (p - 1) * (q - 1)
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
+
+
+def gcd(a: int, b: int) -> int:
+    """
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    while a != 0 and b != 0:
+        if a >= b:
+            a %= b
+        else:
+            a %= b
+    return a or b
+
+
+def multiplicative_inverse(e: int, phi: int) -> int:
+    """
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    # PUT YOUR CODE HERE
+    pass
