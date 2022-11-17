cbc4903a9dde1d2473320c8ef5a125a7f3359334
soreloserrr <112977290+soreloserrr@users.noreply.github.com>
2022-11-17 14:08:35 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,16 +1,16 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
-        Encrypts plaintext using a Caesar cipher.
+    Encrypts plaintext using a Caesar cipher.
 
-        >>> encrypt_caesar("PYTHON")
-        'SBWKRQ'
-        >>> encrypt_caesar("python")
-        'sbwkrq'
-        >>> encrypt_caesar("Python3.6")
-        'Sbwkrq3.6'
-        >>> encrypt_caesar("")
-        ''
-        """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
     ciphertext = ""
     for letters in plaintext:
         if letters.isalpha():
@@ -32,17 +32,17 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
-        Decrypts a ciphertext using a Caesar cipher.
+    Decrypts a ciphertext using a Caesar cipher.
 
-        >>> decrypt_caesar("SBWKRQ")
-        'PYTHON'
-        >>> decrypt_caesar("sbwkrq")
-        'python'
-        >>> decrypt_caesar("Sbwkrq3.6")
-        'Python3.6'
-        >>> decrypt_caesar("")
-        ''
-        """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
     plaintext = ""
     for letters in ciphertext:
         if letters.isalpha():
