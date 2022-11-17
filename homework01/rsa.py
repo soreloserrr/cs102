21c471605558441495213a2e5e07cd1c532d0347
soreloserrr <ekaterina.buyakova00@mail.ru>
2022-11-17 14:53:37 +0300

    tests added 2.0

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -10,6 +10,8 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
+    if n < 0:
+        return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
