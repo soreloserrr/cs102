048ec3fabf6f7f05e7239742b04448ef285d4caa
soreloserrr <ekaterina.buyakova00@mail.ru>
2022-11-17 13:40:49 +0300

    описание исправлений

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -1,5 +1,5 @@
-import typing as tp
 import math
+import typing as tp
 
 
 def check_number(num):
@@ -7,8 +7,8 @@ def check_number(num):
         num = float(num)
         return num
     except ValueError:
-        print('Введен некорректный символ')
-        return 'wrong'
+        print("Введен некорректный символ")
+        return "wrong"
 
 
 def check_int(number: float) -> tp.Union[None, int]:
@@ -17,9 +17,10 @@ def check_int(number: float) -> tp.Union[None, int]:
     return None
 
 
-def math_degree(num1: float, num2: float) -> str:
+def math_degree(num1: float, num2: float) -> tp.Union[str, float]:
     numbers = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     res = ""
+    num1, num2 = int(num1), int(num2)
     while num1 > 0:
         res = numbers[num1 % num2] + res
         num1 = num1 // num2
@@ -49,7 +50,7 @@ def match_case_calc2(num1: float, num2: float, command: str) -> tp.Union[float, 
         case "*":
             return num1 * num2
         case "**":
-            return num1 ** num2
+            return num1**num2
         case _:
             return f"Неизвестный оператор: {command!r}."
 
@@ -63,11 +64,11 @@ def match_case_calc1(num1: float, command: str) -> tp.Union[float, str]:
         case "tan":
             return math.tan(num1)
         case "log10":
-            if num1<= 0:
+            if num1 <= 0:
                 return "Число должно быть больше нуля!"
             return math.log10(num1)
         case "ln":
-            if num1<= 0:
+            if num1 <= 0:
                 return "Число должно быть больше нуля!"
             return math.log(num1)
         case _:
@@ -81,14 +82,14 @@ if __name__ == "__main__":
             break
         if COMMAND == "sin" or COMMAND == "cos" or COMMAND == "tan" or COMMAND == "log10" or COMMAND == "ln":
             NUM_1 = input("Введите число: ")
-            while check_number(NUM_1) == 'wrong':
+            while check_number(NUM_1) == "wrong":
                 NUM_1 = input("Введите число: ")
             print(match_case_calc1(check_number(NUM_1), COMMAND))
         else:
             NUM_1 = input("Введите число: ")
-            while check_number(NUM_1) == 'wrong':
+            while check_number(NUM_1) == "wrong":
                 NUM_1 = input("Введите число: ")
             NUM_2 = input("Введите еще одно число: ")
-            while check_number(NUM_2) == 'wrong':
+            while check_number(NUM_2) == "wrong":
                 NUM_2 = input("Введите число: ")
             print(match_case_calc2(check_number(NUM_1), check_number(NUM_2), COMMAND))
